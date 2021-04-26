62f935456a1b2216eb6051432bde8df05e3642ac
Dimas <51770543+dimas3007@users.noreply.github.com>
2021-04-26 21:18:59 +0700

    News Website

--- index.php
+++ index.php
@@ -2,9 +2,273 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
-</head>
+    <title>Sadam | Blog</title>
+
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></head>
 <body>
-    <h1>Hello World</h1>
+
+    <div class="col-md-12 border-bottom">
+        <div class="container">
+            <div class="py-4 px-4 d-flex justify-content-between">
+                <div class="d-flex">
+                    <h3 class="font-weight-bold text-danger">IDN</h3>
+                    <h3>TIMES</h3>
+                </div>
+                <div></div>
+            </div>
+            <div class="px-4 d-flex">
+                <ul class="d-flex p-0" style="list-style: none">
+                    <li class="font-weight-bold mr-3">#ITEM 1</li>
+                    <li class="font-weight-bold mx-3">ITEM 2</li>
+                    <li class="font-weight-bold mx-3">ITEM 3</li>
+                    <li class="font-weight-bold mx-3">ITEM 4</li>
+                    <li class="font-weight-bold mx-3">ITEM 6</li>
+                    <li class="font-weight-bold mx-3">ITEM 7</li>
+                    <li class="font-weight-bold ml-3">ITEM 8</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="px-4 py-2 d-flex border-bottom border-left border-right">
+            <div class="text-danger font-weight-bold border-right pr-3">Trending</div>
+            <div class="px-3">
+                <a href="#" class="text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt?</a>
+            </div>
+        </div>
+        <div class="border-bottom border-2 pt-5 pb-2">
+            <div class="d-flex align-items-center">
+                <div class="pr-3 border-right border-dark">
+                    <h3 class="font-weight-bold m-0"> NEWS </h3>
+                </div>
+                <div>
+                    <ul class="d-flex m-0" style="list-style: none">
+                        <li><h4 class="mb-0 mr-4">ALL</h4></li>
+                        <li class="text-danger"><h4 class="mb-0 mr-4">INDONESIA</h4></li>
+                        <li><h4 class="mb-0 mr-4">WORLD</h4></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="row py-3">
+            <div class="col-md-7">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://placeimg.com/640/480/any" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://placeimg.com/640/480/any" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://placeimg.com/640/480/any" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="border-bottom d-flex py-2">
+                    <img src="https://placeimg.com/640/480/any" alt="" width="75" height="75">
+                    <div class="px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, earum facilis iure non reiciendis voluptatibus.
+                    </div>
+                </div>
+                <div class="border-bottom d-flex py-2">
+                    <img src="https://placeimg.com/640/480/any" alt="" width="75" height="75">
+                    <div class="px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, earum facilis iure non reiciendis voluptatibus.
+                    </div>
+                </div>
+                <div class="border-bottom d-flex py-2">
+                    <img src="https://placeimg.com/640/480/any" alt="" width="75" height="75">
+                    <div class="px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, earum facilis iure non reiciendis voluptatibus.
+                    </div>
+                </div>
+                <div class="border-bottom d-flex py-2">
+                    <img src="https://placeimg.com/640/480/any" alt="" width="75" height="75">
+                    <div class="px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, earum facilis iure non reiciendis voluptatibus.
+                    </div>
+                </div>
+                <div class="border-bottom d-flex py-2">
+                    <img src="https://placeimg.com/640/480/any" alt="" width="75" height="75">
+                    <div class="px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, earum facilis iure non reiciendis voluptatibus.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row py-4 border-top">
+            <div class="col-md-8">
+                <div class="w-100 text-center">
+                    <u><h3 class="font-weight-bold">BERITA TERKINI</h3></u>
+                </div>
+                <div class="row pt-2">
+                    <div class="col-md-6 my-1">
+                        <div style="height: 250px; width: 350px">
+                            <img src="https://placeimg.com/640/480/any" alt="" width="350px" height="250px">
+                        </div>
+                        <div class="py-1">
+                            <span class="text-danger font-weight-bold">Sport </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                        </div>
+                        <div class="py-1">
+                            <h5>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="col-md-6 my-1">
+                        <div style="height: 250px; width: 350px">
+                            <img src="https://placeimg.com/640/480/any" alt="" width="350px" height="250px">
+                        </div>
+                        <div class="py-1">
+                            <span class="text-danger font-weight-bold">News </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                        </div>
+                        <div class="py-1">
+                            <h5>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, modi!
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="py-4">
+                    <div class="d-flex border-bottom py-3">
+                        <div>
+                            <img src="https://placeimg.com/640/480/any" alt="" width="250px" height="150px">
+                        </div>
+                        <div class="px-2 py-2">
+                            <span class="text-danger font-weight-bold">Sport </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus eveniet explicabo!</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex border-bottom py-3">
+                        <div>
+                            <img src="https://placeimg.com/640/480/any" alt="" width="250px" height="150px">
+                        </div>
+                        <div class="px-2 py-2">
+                            <span class="text-danger font-weight-bold">Sport </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus eveniet explicabo!</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex border-bottom py-3">
+                        <div>
+                            <img src="https://placeimg.com/640/480/any" alt="" width="250px" height="150px">
+                        </div>
+                        <div class="px-2 py-2">
+                            <span class="text-danger font-weight-bold">Sport </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus eveniet explicabo!</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex border-bottom py-3">
+                        <div>
+                            <img src="https://placeimg.com/640/480/any" alt="" width="250px" height="150px">
+                        </div>
+                        <div class="px-2 py-2">
+                            <span class="text-danger font-weight-bold">Sport </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus eveniet explicabo!</h5>
+                        </div>
+                    </div>
+                    <div class="d-flex border-bottom py-3">
+                        <div>
+                            <img src="https://placeimg.com/640/480/any" alt="" width="250px" height="150px">
+                        </div>
+                        <div class="px-2 py-2">
+                            <span class="text-danger font-weight-bold">Sport </span>
+                            <span style="color: gray">~ 26 Apr 21 | 20.23</span>
+                            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus eveniet explicabo!</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border rounded">
+                    <ul style="list-style: none" class="px-4 py-2">
+                        <li class="border-bottom font-weight-bold py-3"> ITEM </li>
+                        <li class="border-bottom font-weight-bold py-3"> ITEM </li>
+                        <li class="border-bottom font-weight-bold py-3"> ITEM </li>
+                        <li class="border-bottom font-weight-bold py-3"> ITEM </li>
+                    </ul>
+                    <div style="background: gray" class="px-4 py-2 text-light">
+                        <div class="d-flex justify-content-center">
+                            <div class="border-bottom py-3">
+                                <span class="mx-2">FOLLOW US :</span>
+                                <i class="fa fa-facebook-square mx-2"></i>
+                                <i class="fa fa-instagram mx-2"></i>
+                                <i class="fa fa-youtube mx-2"></i>
+                                <i class="fa fa-twitter mx-2"></i>
+                            </div>
+                        </div>
+                        <div class="text-center py-3">
+                            @ 2021 Lorem ipsum. | Lorem ipsum dolor sit. " Lorem ipsum dolor. "<br>
+                            <span class="font-weight-bold">#HASTAGNYA</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-12 text-light" style="background: gray">
+        <div class="container py-4">
+            <div class="border-bottom">
+                <ul class="d-flex" style="list-style: none">
+                    <li class="mx-4"><h5>ITEM</h5></li>
+                    <li class="mx-4"><h5>ITEM</h5></li>
+                    <li class="mx-4"><h5>ITEM</h5></li>
+                    <li class="mx-4"><h5>ITEM</h5></li>
+                </ul>
+            </div>
+            <div class="py-3 d-flex justify-content-between">
+                <div>@ 2021 Lorem ipsum. | Lorem ipsum dolor sit.</div>
+                <div>" Lorem ipsum dolor. "</div>
+                <div> <span class="mx-2">FOLLOW US :</span>
+                    <i class="fa fa-facebook-square mx-2"></i>
+                    <i class="fa fa-instagram mx-2"></i>
+                    <i class="fa fa-youtube mx-2"></i>
+                    <i class="fa fa-twitter mx-2"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
 </body>
 </html>
